ae91faed10ce6044bc18cadb44e489a6d9b307e6
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-09-26 10:09:30 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is ChatBot!");
+        System.out.println("I was created in 2024.");
+    }
+}

af03e502e1b8fbed41f9b5cabd49fbffd70a2ea2
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-09-26 10:41:55 +0300

    ChatBot 3-st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -8,8 +8,16 @@ public class ChatBot {
         System.out.println("Hello! My name is ChatBot!");
         System.out.println("I was created in 2024.");
         System.out.println("Please, reming me your name.");
+
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("What a great name you have," + name);
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int num1 = scanner2.nextInt();
+        int num2 = scanner2.nextInt();
+        int num3 = scanner2.nextInt();
+        System.out.println("Your age is "+ ((num1*70+num2*21+num3*15)%105) + "; that`s a good time to start proggraming");
     }
 }

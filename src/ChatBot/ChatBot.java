5104279767324fd6e5ad75bfdf531a9067fffa3d
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-09-26 10:14:26 +0300

    ChatBot 2-st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,15 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
 
     public static void main(String[] args) {
         System.out.println("Hello! My name is ChatBot!");
         System.out.println("I was created in 2024.");
+        System.out.println("Please, reming me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have," + name);
     }
 }

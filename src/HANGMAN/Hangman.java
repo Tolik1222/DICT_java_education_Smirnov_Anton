a38a528e975c54e9aad40180ebc15f95fd5ed87d
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-10-03 10:04:09 +0300

    2-st stage

--- src/HANGMAN/Hangman.java
+++ src/HANGMAN/Hangman.java
@@ -1,9 +1,20 @@
 package HANGMAN;
 
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        System.out.print("Guess the word: ");
+        String guess = scanner.nextLine();
+
+        String word = "python"; // Встановлене слово для гри
+
+        if (guess.equals(word)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
-

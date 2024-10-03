236d02b695de7aa3341b447e721f8bb6da6e9b02
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-10-03 10:08:27 +0300

    5-st stage

--- src/HANGMAN/Hangman.java
+++ src/HANGMAN/Hangman.java
@@ -9,14 +9,32 @@ public class Hangman {
         Random random = new Random();
         String word = words[random.nextInt(words.length)];
 
-        String hint = word.substring(0, 2) + "-".repeat(word.length() - 2);
-        System.out.println("HANGMAN");
-        System.out.println("Guess the word: " + hint);
+        char[] hiddenWord = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            hiddenWord[i] = '-';
+        }
 
+        int attempts = 8;
         Scanner scanner = new Scanner(System.in);
-        String guess = scanner.nextLine();
 
-        if (guess.equals(word)) {
+        while (attempts > 0 && new String(hiddenWord).contains("-")) {
+            System.out.println(hiddenWord);
+            System.out.println("Input a letter: ");
+            char guess = scanner.next().charAt(0);
+
+            if (word.indexOf(guess) >= 0) {
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == guess) {
+                        hiddenWord[i] = guess;
+                    }
+                }
+            } else {
+                System.out.println("That letter doesn't appear in the word.");
+                attempts--;
+            }
+        }
+
+        if (!new String(hiddenWord).contains("-")) {
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");
